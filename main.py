1db68cefecaf45f47c2d6bb46818ee7edee3426f
Mikhail Prozorov <mikepro@alfamail.ru>
2021-04-02 15:53:26 +0300

    Add Grade class as parent to Students and Lecturers in order to make compare

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-class Student:
+class Grades:
+    def __gt__(self, other):
+        return self.avg_grade() > other.avg_grade()
+
+    def __lt__(self, other):
+        return self.avg_grade() < other.avg_grade()
+
+    def __ge__(self, other):
+        return self.avg_grade() >= other.avg_grade()
+
+    def __le__(self, other):
+        self.avg_grade() <= other.avg_grade()
+
+
+class Student(Grades):
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -41,11 +55,17 @@ class Mentor:
 
 
 
-class Lecturer(Mentor):
+class Lecturer(Grades, Mentor):
     def __init__(self, *args):
         self.grades = {}
         super(Lecturer, self).__init__(*args)
         pass
+
+    def __cmp__(self, other):
+        if isinstance(other, Lecturer):
+            return self.avg_grade() - other.avg_grade()
+        else:
+            return None
 
     def avg_grade(self):
         avg = 0
@@ -76,20 +96,29 @@ class Reviewer(Mentor):
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
+best_student1 = Student('Homer', 'Simpson', 'male')
+best_student1.courses_in_progress += ['Python']
+
 cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_lecturer = Lecturer('Another', 'Buddy')
 cool_lecturer.courses_attached += ['Python']
 best_student.rate_lecturer(cool_lecturer, 'Python', 10)
+
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+cool_mentor.rate_hw(best_student1, 'Python', 5)
+cool_mentor.rate_hw(best_student1, 'Python', 5)
+cool_mentor.rate_hw(best_student1, 'Python', 5)
+
+print(best_student > best_student1)
+
+
 print(cool_lecturer.grades)
-
 print(cool_lecturer.name, cool_lecturer.surname, cool_lecturer.courses_attached)
-
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
 print(best_student.grades)
 print(best_student)
